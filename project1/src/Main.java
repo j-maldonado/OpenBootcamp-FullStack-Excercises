f1c5de319b9aa4ee04e542fd60ab70c011400d77
Joan Maldonado <joanmarcelomaldonado@gmail.com>
2022-12-04 19:08:30 -0300

    Ejercicio1 Primera parte
    Crear una función con tres parámetros que sean números que se suman entre sí.
    
    Llamar a la función en el main y darle valores.

---
+++ project1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ejercicio1(10, 20, 30);
+    }
+    public static void ejercicio1(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+}
